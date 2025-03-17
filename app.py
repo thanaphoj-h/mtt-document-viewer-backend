b275d616cd119bfc5a7d83a9d70bf82888e1030c
Thanaphoj Horthaisong <thanaphoj.horthaisong@gmail.com>
2025-03-17 15:36:48 +0700

    Added logger to app.py

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from component.logger import logger
 from fastapi import FastAPI
 
 app = FastAPI()
@@ -5,4 +6,5 @@ app = FastAPI()
 # Root Endpoint
 @app.get("/")
 def root():
+    logger.info(f"Access Root Endpoint")
     return {"message": "Root Endpoint of MTT Document Viewer API"}
